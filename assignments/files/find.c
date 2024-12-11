64a889dc8389df9c3c031dae2cd97536bcc53a9f
JSH32 <rudnik.sputnik@gmail.com>
2024-12-11 22:39:09 +0000

    Better find (singlethreaded)

--- assignments/files/find.c
+++ assignments/files/find.c
@@ -4,50 +4,86 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+// THIS IS A BETTER IMPLEMENTATION
+// Uploaded after my original submission just to see if can be made better
+// There is an original implementation before due date so handgraders can grade
+// that one if needed. Tis is for me (and you if you want)
+
 #define MAX_WORD_LENGTH 1000
 #define BUFFER_SIZE 4096 * 16
 
 int count_word_in_file(const char* filename, const char* search_word) {
+  // Open file
   FILE* file = fopen(filename, "r");
   if (file == NULL) {
-    printf("Error opening file: %s\n", filename);
     return -1;
   }
 
   char buffer[BUFFER_SIZE + 1];
-  int count       = 0;
-  size_t word_len = strlen(search_word);
+  int count    = 0;
+  int word_len = strlen(search_word);
+
+  char leftover[MAX_WORD_LENGTH] = { 0 };
+  int leftover_len               = 0;
 
   while (1) {
     // Read chunk of the file
-    size_t bytes_read = fread(buffer, 1, BUFFER_SIZE, file);
-    if (bytes_read == 0)
+    int bytes_read = fread(buffer, 1, BUFFER_SIZE, file);
+    if (bytes_read == 0) {
       break;
+    }
 
-    // Null-terminate buffer
     buffer[bytes_read] = '\0';
 
-    // Substring search
-    for (size_t i = 0; i <= bytes_read - word_len; i++) {
+    // Combine leftover and the buffer
+    char combined[BUFFER_SIZE + MAX_WORD_LENGTH + 1];
+    for (int i = 0; i < leftover_len; i++) {
+      combined[i] = leftover[i];
+    }
+    for (int i = 0; i < bytes_read; i++) {
+      combined[leftover_len + i] = buffer[i];
+    }
+
+    int combined_len       = leftover_len + bytes_read;
+    combined[combined_len] = '\0';
+
+    // Search for the word manually in the combined array
+    for (int i = 0; i <= combined_len - word_len; i++) {
       int match = 1;
-      for (size_t j = 0; j < word_len; j++) {
-        if (buffer[i + j] != search_word[j]) {
+      for (int j = 0; j < word_len; j++) {
+        if (combined[i + j] != search_word[j]) {
           match = 0;
           break;
         }
       }
       if (match) {
         count++;
-        i += word_len - 1; // Skip to end of current match
+        i += word_len - 1; // Skip to the end of the word
       }
     }
 
-    if (bytes_read < BUFFER_SIZE)
+    // Update leftover for the next iteration
+    if (combined_len >= word_len) {
+      leftover_len = word_len - 1;
+      for (int i = 0; i < leftover_len; i++) {
+        leftover[i] = combined[combined_len - word_len + 1 + i];
+      }
+      leftover[leftover_len] = '\0';
+    } else {
+      leftover_len = combined_len;
+      for (int i = 0; i < leftover_len; i++) {
+        leftover[i] = combined[i];
+      }
+      leftover[leftover_len] = '\0';
+    }
+
+    if (bytes_read < BUFFER_SIZE) {
       break;
+    }
   }
 
-  // You really should close the file but it says we can't use it lmao.
-  // fclose(file);
+  // Close the file
+  fclose(file);
   return count;
 }
 
